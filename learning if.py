7c95fb61c08ca6b1b89632a19a366fe6de37f37e
Chandler Huth <chandlerhuth@Alveyworld-13.local>
2015-09-14 10:19:16 -0600

    learning if statement

---
+++ learning if.py
@@ -0,0 +1,13 @@
+# learning about if statements
+number = int(raw_input("Enter Your Age"))
+
+if number < 5:
+	print "You are not in School"
+elif number < 11:
+	print "You are in Elementary School"
+elif number < 15:
+	print "Lmao you're like 12"
+elif number < 19:
+	print "You are in High School"
+else:
+	print "You know everything you smart silly pants"
